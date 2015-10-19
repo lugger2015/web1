98b71f44850086fc3d0556612c785dd4c5368552
lugger2015 <lugger2015@ya.ru>
2015-10-19 23:02:52 +0300

    Added changes

--- js/glsl_program.js
+++ js/glsl_program.js
@@ -1,55 +1,8 @@
-var GLSLprogram_ = function (gl) {
-    this.gl = gl;
-    this.linked = false;
-    this.uniformLocation = {};
-    this.handle = this.gl.createProgram();
-    if (!this.handle) {
-        return null;
-    }
-};
-
-GLSLprogram_.prototype.getUniformLocation = function (uniformName) {
-    if (this.uniformLocation.hasOwnProperty(uniformName)) {
-        return this.uniformLocation[uniformName];
-    }
-};
-
-GLSLprogram_.prototype.compileShader = function (shaderText, shaderType) {
-    var shader = this.gl.createShader(shaderType);
-    if (!shader) {
-        console.log('невозможно создать шейдер');
-        return null;
-    }
-    this.gl.shaderSource(shader, shaderText);
-    this.gl.compileShader(shader);
-    var compiled = this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS);
-    if (!compiled) {
-        var error = this.gl.getShaderInfoLog(shader);
-        console.log('Ошибки при компиляции шейдера:' + error);
-        this.gl.deleteShader(shader);
-        return null;
-    }
-    this.gl.attachShader(this.handle, shader);
-};
-
-GLSLprogram_.prototype.link = function () {
-    this.gl.linkProgram(this.handle);
-    var linkStatus = this.gl.getProgramParameter(this.handle, this.gl.LINK_STATUS);
-    if (!linkStatus) {
-        var error = this.gl.getProgramInfoLog(this.handle);
-        console.log('Ошибки при сборке программы' + error);
-        this.gl.deleteProgram(this.handle);
-        // this.gl.deleteShader(vertexShader);
-        // this.gl.deleteShader(fragmentShader);
-        return null;
-    }
-    return this.handle;
-};
-
 var GLSLProgram = {
     init: function (gl) {
         this.gl = gl;
         this.handle = this.gl.createProgram();
+        this.uniformLocation = {};
     },
     compileShader: function (shaderText, shaderType) {
         var shader = this.gl.createShader(shaderType);
@@ -80,10 +33,18 @@ var GLSLProgram = {
             return null;
         }
         return this.handle;
+    },
+    setUniform4f: function (uniformName,x,y,z,w) {
+        if (this.uniformLocation.hasOwnProperty(uniformName)) {
+            this.gl.uniform4f(this.uniformLocation[uniformName],x,y,z,w);
+        } else {
+            var u_location = this.gl.getUniformLocation(this.handle, uniformName);
+            if (!u_location) {
+                console.log('невозможно получить uniform с именем '+u_location);
+                return null;
+            }
+            this.uniformLocation[uniformName] = u_location;
+            this.gl.uniform4f(u_location,x,y,z,w);
+        }
     }
-
 };
-
-// var program = new GLSLprogram_();
-// program.uniformLocation.hello = "world";
-// console.log(program.getUniformLocation("hello"));
--- js/script2.js
+++ js/script2.js
@@ -1,49 +1,40 @@
-
 shaderSources = {
-    vShaderSource : null,
-    fShaderSource : null
+    vShaderSource: null,
+    fShaderSource: null
 };
 
-
-function init () {
-    getSource("shader.vert",shaderSources,'vShaderSource');
-    getSource("shader.frag",shaderSources,'fShaderSource');
+function init() {
+    getSource("shader.vert", shaderSources, 'vShaderSource');
+    getSource("shader.frag", shaderSources, 'fShaderSource');
 }
 
 function main() {
     var canvas = document.getElementById('canvas');
     var gl = canvas.getContext('webgl');
-    if ( !gl ) {
+    if (!gl) {
         console.log('не возможно получить контекст');
         return;
     }
-    gl.clearColor(0.0,0.0,1.0,1.0);
+    gl.clearColor(0.0, 0.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    //var program = new GLSLprogram_(gl);
-    // var program = createProgram(gl, shaderSources.vShaderSource, shaderSources.fShaderSource);
     var program = Object.create(GLSLProgram);
     program.init(gl);
-    program.compileShader(shaderSources.vShaderSource,gl.VERTEX_SHADER);
-    program.compileShader(shaderSources.fShaderSource,gl.FRAGMENT_SHADER);
+    program.compileShader(shaderSources.vShaderSource, gl.VERTEX_SHADER);
+    program.compileShader(shaderSources.fShaderSource, gl.FRAGMENT_SHADER);
     program.link();
     var a_Position = gl.getAttribLocation(program.handle, 'a_Position');
 
-    if ( a_Position < 0 ) {
+    if (a_Position < 0) {
         console.log('невозможно получить a_Position');
         return null;
     }
-    var u_FragColor = gl.getUniformLocation(program.handle, 'u_FragColor');
-    if (!u_FragColor) {
-        console.log('невозможно получить u_FragColor');
-        return;
-    }
+
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program.handle);
-    gl.uniform4f(u_FragColor,Math.random(),Math.random(),Math.random(),1.0);
-    
+    program.setUniform4f('u_FragColor', 1.0, 0.0, 0.0, 1.0);
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    var vertices = new Float32Array([0.0,0.5,-0.5,-0.5,0.5,-0,5]);
+    var vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0, 5]);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_Position);
@@ -51,20 +42,20 @@ function main() {
 
 }
 
-function getSource (filename,sources,name) {
+function getSource(filename, sources, name) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status !== 404) { 
+        if (request.readyState === 4 && request.status !== 404) {
             sources[name] = request.responseText;
             initStatus.change();
         }
     };
-    request.open('GET',filename,true);
+    request.open('GET', filename, true);
     request.send();
 }
 
 initStatus = {
-    change: function() {
+    change: function () {
         for (var item in shaderSources) {
             if (shaderSources.hasOwnProperty(item)) {
                 if (!shaderSources[item]) return;
